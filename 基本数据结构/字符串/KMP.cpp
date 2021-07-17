80f34895e2186d49357cda86776679956a2f2660
jirunduan(段吉润) <jirunduan@tencent.com>
2021-07-17 18:43:42 +0800

    修改了KMP算法

--- 基本数据结构/字符串/KMP.cpp
+++ 基本数据结构/字符串/KMP.cpp
@@ -1,41 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	string s; //待匹配的字符串
-	string p; //模式串
-	cin >> s >> p;
-	int m = p.length();
-	//先从模式串构建DP数组，模式串的前缀[0,m]的后缀与p匹配的最长真前缀的最后一个字符的下标。
-	int pi[m];
-	int k = -1;
-	pi[0] = -1;
-	for (int i = 1; i < m; i++)
-	{
-		while (k != -1 && p[i] != p[k + 1])
-			k = pi[k];
-		if (p[i] == p[k + 1])
-			k++;
-		pi[i] = k;
-	}
-	for (int i = 0; i < m; i++)
-		cout << pi[i] << " ";
-	cout << endl;
-	k = -1;
-	//模式串匹配
-	int n = s.length();
-	for (int i = 0; i < n; i++)
-	{
-		while (k != -1 && s[i] != p[k + 1])
-			k = pi[k];
-		if (s[i] == p[k + 1])
-			k++;
-		if (k == m - 1)
-		{
-			cout << i << " ";
-			k = pi[k];
-		}
-	}
-	return 0;
+int main(){
+    string s, p;
+    cin >> s >> p;
+    int n = s.length();
+    int m = p.length();
+    int pi[m];
+    int k = -1;
+    pi[0] = -1;
+    //什么逻辑呢？
+    //首先pi[k]表示的是p与子串p[0,k]的真后缀匹配的字符串的最大长度是多少，不过这里我们保存的是最长前缀的最后一个元素的下标。
+    //p与自身匹配的过程
+    for (int i = 1; i < m; i++){
+        while(k!=-1&&p[i]!=p[k+1])
+            k = pi[k];
+        if(p[i]==p[k+1])
+            k++;
+        pi[i] = k;
+    }
+    //p与s的匹配过程，也即这里是两个单独的匹配过程逻辑是一致的，但是与自身匹配时需要记录pi，与s匹配时只需要匹配即可。
+    k = -1;
+    for (int i = 0; i < n; i++){
+        while(k!=-1&&s[i]!=p[k+1])
+            k = pi[k];
+        if(s[i] == p[k+1])
+            k++;
+        if(k == m-1){
+            cout << i - k +1 << endl;
+            k = pi[k];
+        }
+    }
+    for (int i = 0; i < m; i++){
+        cout << pi[i] + 1 << " ";
+    }
+    return 0;
 }
